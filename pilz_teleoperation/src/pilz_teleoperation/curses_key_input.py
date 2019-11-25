57f7a97fbf0328cb0cb34e8fd96626334321dd8d
Joachim Schleicher <j.schleicher@pilz.de>
2019-11-25 13:19:03 +0100

    added rotation input

--- pilz_teleoperation/src/pilz_teleoperation/curses_key_input.py
+++ pilz_teleoperation/src/pilz_teleoperation/curses_key_input.py
@@ -23,6 +23,30 @@ class CursesKeyInput(TeleoperationInput):
         The move commands are published on a 2D plane that can be toggled between XY, XZ and YZ
         Additional Settings like target frame or velocity scaling are published separately.
     """
+    MOVE_BINDINGS = {
+        ord('7'): Twist(linear=Vector3(-1, 1, 0)),
+        ord('8'): Twist(linear=Vector3(0, 1, 0)),
+        ord('9'): Twist(linear=Vector3(1, 1, 0)),
+        ord('6'): Twist(linear=Vector3(1, 0, 0)),
+        ord('3'): Twist(linear=Vector3(1, -1, 0)),
+        ord('2'): Twist(linear=Vector3(0, -1, 0)),
+        ord('1'): Twist(linear=Vector3(-1, -1, 0)),
+        ord('4'): Twist(linear=Vector3(-1, 0, 0)),
+        
+        ord('u'): Twist(angular=Vector3(-1, 0, 0)),
+        ord('i'): Twist(angular=Vector3(0, -1, 0)),
+        ord('o'): Twist(angular=Vector3(0, 0, -1)),
+        ord('j'): Twist(angular=Vector3(1, 0, 0)),
+        ord('k'): Twist(angular=Vector3(0, 1, 0)),
+        ord('l'): Twist(angular=Vector3(0, 0, 1)),
+    }
+    SETTING_BINDINGS = {
+        ord('+'): SetTeleopSettingsRequest.INCREASE_LINEAR_VELOCITY,
+        ord('-'): SetTeleopSettingsRequest.DECREASE_LINEAR_VELOCITY,
+        ord('*'): SetTeleopSettingsRequest.TOGGLE_PLANE,
+        ord('/'): SetTeleopSettingsRequest.TOGGLE_WORLD_AND_TCP_FRAME,
+        ord(','): SetTeleopSettingsRequest.TOGGLE_CONTROLLER  # not yet implemented
+    }
 
     def __init__(self, stdscr, *args, **kwargs):
         super(CursesKeyInput, self).__init__(*args, **kwargs)
@@ -42,5 +66,8 @@ class CursesKeyInput(TeleoperationInput):
 
     def _read_keyboard_input(self):
         key_code = self._screen.getch()
-        curses.flushinp()
-        return key_code
+        if key_code in CursesKeyInput.MOVE_BINDINGS:
+            self._twist_pub.publish(CursesKeyInput.MOVE_BINDINGS[key_code])
+        elif key_code in CursesKeyInput.SETTING_BINDINGS:
+            self._setting_srv(SetTeleopSettingsRequest(pressed_commands=[CursesKeyInput.SETTING_BINDINGS[key_code]]))
+

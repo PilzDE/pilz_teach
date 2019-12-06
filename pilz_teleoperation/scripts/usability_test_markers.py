e9e1f9382b01779adaa74ce1d6a85156fb2333bb
tobi-v <t_vetter@live.de>
2019-12-06 13:47:31 +0100

    Put everythin in a dedicated class

--- pilz_teleoperation/scripts/usability_test_markers.py
+++ pilz_teleoperation/scripts/usability_test_markers.py
@@ -18,39 +18,44 @@
 import rospy
 from visualization_msgs.msg import Marker
 
+class UsabilityTestMarkerPublisher(object):
 
-def setup_generic_marker():
-    m = Marker()
-    m.action = Marker.ADD
-    m.header.frame_id = '/world'
-    m.header.stamp = rospy.Time.now()
-    m.ns = 'usability_test_marker'
-    m.id = 0
-    m.type = Marker.ARROW
-    m.pose.position.x = 1.0
-    m.pose.position.y = 1.0
-    m.pose.position.z = 1.0
-    m.pose.orientation.w = 1
-    m.scale.x = 0.15
-    m.scale.y = 0.01
-    m.scale.z = 0.01
-    m.color.r = 1.0
-    m.color.g = 1.0
-    m.color.b = 1.0
-    m.color.a = 1.0
-    return m
+    def __init__(self):
+        self.pub = rospy.Publisher('visualization_marker', Marker, queue_size=10)
+        rospy.init_node('usability_test_markers')
+        self.rate = rospy.Rate(1)
 
-def publish_markers():
-    pub = rospy.Publisher('visualization_marker', Marker, queue_size=10)
-    rospy.init_node('usability_test_markers')
-    rate = rospy.Rate(1)
-    while not rospy.is_shutdown():
-        pub.publish(setup_generic_marker())
-        rate.sleep()
+        self.setup_generic_marker()
 
+    def setup_generic_marker(self):
+        self.m = Marker()
+        self.m.action = Marker.ADD
+        self.m.header.frame_id = '/world'
+        self.m.ns = 'usability_test_marker'
+        self.m.type = Marker.ARROW
+        self.m.color.r = 1.0
+        self.m.color.g = 1.0
+        self.m.color.b = 1.0
+        self.m.color.a = 1.0
+        self.m.scale.x = 0.15
+        self.m.scale.y = 0.01
+        self.m.scale.z = 0.01
+
+        self.m.header.stamp = rospy.Time.now()
+        self.m.id = 0
+        self.m.pose.position.x = 1.0
+        self.m.pose.position.y = 1.0
+        self.m.pose.position.z = 1.0
+        self.m.pose.orientation.w = 1
+
+    def publish_markers(self):
+        while not rospy.is_shutdown():
+            self.pub.publish(self.m)
+            self.rate.sleep()
 
 if __name__ == '__main__':
+    publisher = UsabilityTestMarkerPublisher()
     try:
-        publish_markers()
+        publisher.publish_markers()
     except rospy.ROSInterruptException:
         pass

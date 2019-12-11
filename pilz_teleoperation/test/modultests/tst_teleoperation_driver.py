4868b234419e5f57eec39a371998129e09bffce0
rfeistenauer <r.feistenauer@pilz.de>
2019-12-11 15:52:58 +0100

    patched rostime

--- pilz_teleoperation/test/modultests/tst_teleoperation_driver.py
+++ pilz_teleoperation/test/modultests/tst_teleoperation_driver.py
@@ -7,11 +7,14 @@ from pilz_teleoperation.srv import SetTeleopSettingsRequest
 
 
 class TestTeleoperationDriver(unittest.TestCase):
-    def test_window_update_after_config_change(self):
+    @mock.patch('rospy.rostime.get_rostime')
+    def test_window_update_after_config_change(self, mocked_rostime):
+        mocked_rostime.return_value = 0
         win_mock = mock.MagicMock()
         driver = TeleoperationDriver(window=win_mock)
         self.assertEquals(win_mock.driver_settings_changed.call_count, 1)
-        driver.set_teleop_settings([SetTeleopSettingsRequest.TOGGLE_VELOCITY_UP])
+        driver.set_teleop_settings(SetTeleopSettingsRequest(
+            pressed_commands=[SetTeleopSettingsRequest.TOGGLE_VELOCITY_UP]))
         self.assertEquals(win_mock.driver_settings_changed.call_count, 2)
 
 

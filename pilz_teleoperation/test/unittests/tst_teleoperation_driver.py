c1ae2a4fc3ebabeeb7345a4ed66ba60d7064be44
tobi-v <t_vetter@live.de>
2020-01-02 18:47:55 +0100

    Replaces no-longer supported nosetest with py_test

--- pilz_teleoperation/test/unittests/tst_teleoperation_driver.py
+++ pilz_teleoperation/test/unittests/tst_teleoperation_driver.py
@@ -1,25 +1,18 @@
 #!/usr/bin/env python
 
-import unittest
 import mock
 from pilz_teleoperation import TeleoperationDriver
 from pilz_teleoperation.srv import SetTeleopSettingsRequest
 
-import pytest
+import ros_pytest
 
-
-class TestTeleoperationDriver(unittest.TestCase):
+class TestTeleoperationDriver():
     @mock.patch('rospy.rostime.get_rostime')
     def test_window_update_after_config_change(self, mocked_rostime):
         mocked_rostime.return_value = 0
         win_mock = mock.MagicMock()
         driver = TeleoperationDriver(window=win_mock)
-        self.assertEquals(win_mock.driver_settings_changed.call_count, 1)
+        assert win_mock.driver_settings_changed.call_count == 1
         driver.set_teleop_settings(SetTeleopSettingsRequest(
             pressed_commands=[SetTeleopSettingsRequest.TOGGLE_JOINT_UP]))
-        self.assertEquals(win_mock.driver_settings_changed.call_count, 2)
-
-
-if __name__ == '__main__':
-    import rosunit
-    rosunit.unitrun('pilz_teleoperation', 'test_teleoperation_driver', TestTeleoperationDriver)
+        assert win_mock.driver_settings_changed.call_count == 2

68565e28066c25bf91344275b66ea6f05c3bdcc0
rfeistenauer <r.feistenauer@pilz.de>
2020-01-20 10:11:30 +0100

    added tests for teleoperation driver

--- pilz_teleoperation/src/pilz_teleoperation/teleoperation_driver.py
+++ pilz_teleoperation/src/pilz_teleoperation/teleoperation_driver.py
@@ -14,25 +14,16 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import rospy
-import math
 import copy
 import pilz_teleoperation.teleoperation_settings as _teleop_settings
 
-from geometry_msgs.msg import Twist, TwistStamped, Vector3
+from geometry_msgs.msg import Twist, TwistStamped
 from control_msgs.msg import JointJog
 from pilz_teleoperation.srv import SetTeleopSettings, SetTeleopSettingsResponse
 
 
-class _TeleoperationTwist(object):
+class _TeleoperationTwist(Twist):
     """ Extension of Twist class to store the required math. """
-    def __init__(self, twist=None, *args, **kwargs):
-        super(_TeleoperationTwist, self).__init__(*args, **kwargs)
-        self.linear = Vector3(twist.linear.x,
-                              twist.linear.y,
-                              twist.linear.z)
-        self.angular = Vector3(twist.angular.x,
-                               twist.angular.y,
-                               twist.angular.z)
 
     def project_on_plane(self, projection_plane):
         first = self.linear.x
@@ -40,16 +31,6 @@ class _TeleoperationTwist(object):
         self.linear.x, self.linear.y = 0, 0
         setattr(self.linear, projection_plane[0], first)
         setattr(self.linear, projection_plane[1], second)
-
-    def change_rotation_axis(self, axis):
-        value = self.angular.x
-        self.angular.x = 0
-        setattr(self.angular, axis, value)
-
-    def normalize(self):
-        norm = math.sqrt(self.linear.x ** 2 + self.linear.y ** 2 + self.linear.z ** 2)
-        if norm > 0:
-            self.scale_linear_velocity(1/norm)
 
     def scale_linear_velocity(self, vel_scale):
         self.linear.x = self.scale_value(self.linear.x, vel_scale)
@@ -61,7 +42,8 @@ class _TeleoperationTwist(object):
         self.angular.y = self.scale_value(self.angular.y, ang_vel_scale)
         self.angular.z = self.scale_value(self.angular.z, ang_vel_scale)
 
-    def scale_value(self, value, scale):
+    @staticmethod
+    def scale_value(value, scale):
         if value == 'max':
             return 1
         elif value == '-max':
@@ -70,13 +52,8 @@ class _TeleoperationTwist(object):
             return value * scale
 
 
-class _TeleoperationJointJog(object):
+class _TeleoperationJointJog(JointJog):
     """ Extension of JointJog class to store required math. """
-    def __init__(self, joint_jog, *args, **kwargs):
-        super(_TeleoperationJointJog, self).__init__(*args, **kwargs)
-        self.joint_names = joint_jog.joint_names
-        self.displacements = joint_jog.displacements
-        self.velocities = joint_jog.velocities
 
     def scale_velocity(self, vel_scale):
         self.velocities = [v * vel_scale for v in self.velocities]
@@ -152,7 +129,7 @@ class TeleoperationDriver(object):
         return st
 
     def _send_updated_twist(self, ts):
-        new_twist = _TeleoperationTwist(twist=ts.twist)
+        new_twist = _TeleoperationTwist(linear=ts.twist.linear, angular=ts.twist.angular)
         new_twist.project_on_plane(self._settings.toggled_plane)
         new_twist.scale_linear_velocity(self._settings.linear_velocity)
         new_twist.scale_angular_velocity(self._settings.angular_velocity)
@@ -160,12 +137,12 @@ class TeleoperationDriver(object):
         self._twist_publisher.publish(ts)
 
     def _send_updated_jog(self, js):
-        js.header.stamp = rospy.Time.now()
-        js.header.frame_id = self._settings.toggled_target_frame
-
-        new_jog = _TeleoperationJointJog(joint_jog=js)
+        new_jog = _TeleoperationJointJog(joint_names=js.joint_names,
+                                         velocities=js.velocities,
+                                         displacements=js.displacements)
+        new_jog.header.stamp = rospy.Time.now()
+        new_jog.header.frame_id = self._settings.toggled_target_frame
         new_jog.choose_joint_to_jog(self._settings.toggled_joint)
         new_jog.copy_jog_data(js)
         new_jog.scale_velocity(self._settings.angular_velocity)
-
-        self._jog_publisher.publish(js)
+        self._jog_publisher.publish(new_jog)
--- pilz_teleoperation/test/unittests/test_teleoperation_driver.py
+++ pilz_teleoperation/test/unittests/test_teleoperation_driver.py
@@ -17,17 +17,60 @@
 
 import rospy
 import mock
+import pytest
 from pilz_teleoperation import TeleoperationDriver
-from pilz_teleoperation.srv import SetTeleopSettingsRequest
+from pilz_teleoperation.teleoperation_driver import _TeleoperationTwist, _TeleoperationJointJog
+from pilz_teleoperation.srv import SetTeleopSettingsRequest, SetTeleopSettingsResponse
 from param_server_mock import mocked_get_param
+from geometry_msgs.msg import TwistStamped, Twist, Vector3
+from std_msgs.msg import Header
+from control_msgs.msg import JointJog
 
 
-def test_window_update_after_config_change(monkeypatch):
-    monkeypatch.setattr(rospy.rostime, "get_rostime", lambda: rospy.Time())
+@pytest.fixture
+def patch_ros(monkeypatch):
+    monkeypatch.setattr(rospy.rostime, "get_rostime", lambda: rospy.Time(secs=5))
     monkeypatch.setattr(rospy, "get_param", mocked_get_param)
+    monkeypatch.setattr(rospy, "Service", mock.MagicMock())
+
+
+def test_window_update_after_config_change(patch_ros):
     win_mock = mock.MagicMock()
     driver = TeleoperationDriver(window=win_mock)
     assert win_mock.driver_settings_changed.call_count == 1
     driver.set_teleop_settings(SetTeleopSettingsRequest(
         pressed_commands=[SetTeleopSettingsRequest.TOGGLE_JOINT_UP]))
     assert win_mock.driver_settings_changed.call_count == 2
+
+
+def test_invalid_setting(patch_ros):
+    driver = TeleoperationDriver(window=mock.MagicMock())
+    response = driver.set_teleop_settings(SetTeleopSettingsRequest(pressed_commands=["invalid"]))
+    assert response == SetTeleopSettingsResponse(success=False, error_msg="Unsupported Command")
+
+
+@pytest.mark.parametrize("method, argument, expected",
+                         [("set_twist_command",
+                           Twist(linear=Vector3(y=1), angular=Vector3(z=1)),
+                           TwistStamped(header=Header(stamp=rospy.Time(secs=5), frame_id="world"),
+                                        twist=_TeleoperationTwist(linear=Vector3(y=0.2), angular=Vector3(z=0.2)))),
+                          ("set_twist_command",
+                           Twist(linear=Vector3(y="max"), angular=Vector3(z="-max")),
+                           TwistStamped(header=Header(stamp=rospy.Time(secs=5), frame_id="world"),
+                                        twist=_TeleoperationTwist(linear=Vector3(y=1.0), angular=Vector3(z=-1.0)))),
+                          ("set_joint_jog_command",
+                           JointJog(joint_names=("joint1", "joint2"), velocities=(1, 1)),
+                           _TeleoperationJointJog(header=Header(stamp=rospy.Time(secs=5), frame_id="world"),
+                                                  joint_names=["joint1", "joint2"], velocities=[.2, .2])),
+                          ("set_joint_jog_command",
+                           JointJog(velocities=[-1]),
+                           _TeleoperationJointJog(header=Header(stamp=rospy.Time(secs=5), frame_id="world"),
+                                                  joint_names=["prbt_joint_1"], velocities=[-.2]))
+                          ])
+def test_twist_modification(method, argument, expected, patch_ros, monkeypatch):
+    driver = TeleoperationDriver(window=mock.MagicMock())
+    jog_server_mock = mock.Mock()
+    monkeypatch.setattr(rospy.Publisher, "publish", jog_server_mock)
+
+    getattr(driver, method)(argument)
+    jog_server_mock.assert_called_with(expected)
